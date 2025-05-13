c5d92b257b41ffc672638790682c81a717c9a607
sl5net <sl5softwarelab@gmail.com>
2025-05-13 11:38:42 +0200

    add CtrlC CtrlV for console and VSCode

---
+++ autokey/CtrlC-is-ShiftCtrlC.py
@@ -0,0 +1,6 @@
+# recomandet hotkey ctrl+c
+# recomandet Window Filter:  code.Code|konsole.konsole
+# may read also https://github.com/sl5net/0ad-autokey-scripts/
+
+keyboard.send_keys("<ctrl>+<shift>+c")
+
---
+++ autokey/CtrlV-is-ShiftCtrlV.py
@@ -0,0 +1,6 @@
+# recomandet hotkey ctrl+v
+# recomandet Window Filter:  code.Code|konsole.konsole
+# may read also https://github.com/sl5net/0ad-autokey-scripts/
+
+keyboard.send_keys("<ctrl>+<shift>+v")
+
